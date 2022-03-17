892abf9d6780217f01b8e0e44e35eac3566f8226
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-17 15:20:00 +0000

    SocialMediaDemo: initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include_once "../global_tools.php";
+$lastUpdate = "22-03-17-1518"; // when changing this, you should also update sw.js
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<?php createHead(
+		"Social Media Demo",
+		"A demo layout for a hypothetical social media, made to try out recreating a native-like app on the web.",
+		null,
+		null,
+		"2022-03-17",
+		"InteractiveResource",
+		false
+	); ?>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<h1>Social Media Demo</h1>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
