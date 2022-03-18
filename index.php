228c8da6e2ff744835da2868f12d8ab3f31b471c
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-18 07:56:45 +0000

    SocialMediaDemo: set default page

--- index.php
+++ index.php
@@ -34,6 +34,12 @@ $lastUpdate = "22-03-17-1628"; // when changing this, you should also update sw.
 </div>
 
 
+<script>
+    // set default page
+    if (location.hash.length <= 1) {
+        document.getElementById("pageCamera").scrollIntoView();
+    }
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="assets/scripts/main.js?lastUpdate=<?=$lastUpdate?>"></script>
 
