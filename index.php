b574c89fb7103f0bfaee3655f05a638b4fe0fa6a
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-18 08:13:56 +0000

    SocialMediaDemo: move camera logic to pageCamera.ts

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $lastUpdate = "22-03-18-0807"; // when changing this, you should also update sw.
     }
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="assets/scripts/main.js?lastUpdate=<?=$lastUpdate?>"></script>
+<script src="assets/scripts/main.js?lastUpdate=<?=$lastUpdate?>" type="module"></script>
 
 </body>
 </html>
