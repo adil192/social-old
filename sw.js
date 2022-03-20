e0b875fe8577685cee60ea8002aceec36a404e72
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-20 18:32:33 +0000

    SocialMediaDemo: add dark mode

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name has a timestamp because the browser re-caches the assets when the service worker file is modified
-const staticCacheName = "SocialMediaDemo-cache-" + "22-03-20-1615";
+const staticCacheName = "SocialMediaDemo-cache-" + "22-03-20-1829";
 const assets = [
 	'/favicon.ico',
 	'/SocialMediaDemo/',
