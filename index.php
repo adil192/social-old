d52997721736066f8c6c0df9b4c0f9002acc0f6a
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-20 19:14:53 +0000

    SocialMediaDemo: better dark mode

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once "../global_tools.php";
-$lastUpdate = "22-03-20-1829"; // when changing this, you should also update sw.js
+$lastUpdate = "22-03-20-1913"; // when changing this, you should also update sw.js
 ?>
 <!doctype html>
 <html lang="en">
