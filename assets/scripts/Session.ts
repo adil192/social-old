adcd8850ee10558ee775633976ed11cceadb4ef6
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-24 03:18:14 +0000

    SocialMediaDemo: Session: save and restore to cookie

--- assets/scripts/Session.ts
+++ assets/scripts/Session.ts
@@ -1,5 +1,7 @@
 
 class SessionStruct {
+	private static readonly cookieName: string = "session=";
+
 	private _isReady: boolean = false;
 	private _isLoggedIn: boolean = false;
 
@@ -7,36 +9,39 @@ class SessionStruct {
 	public set isReady(isReady: boolean) { this._isReady = isReady; this.onchange(); }
 
 	public get isLoggedIn(): boolean { return this._isLoggedIn; }
-	public set isLoggedIn(isLoggedIn: boolean) { this._isLoggedIn = isLoggedIn; }
+	public set isLoggedIn(isLoggedIn: boolean) { this._isLoggedIn = isLoggedIn; this.onchange(); }
 
+	public constructor() {
+		this.restoreFromCookie();
+	}
+
+	saveToCookieTimeout: number = null;
 	private onchange() {
+		clearTimeout(this.saveToCookieTimeout);
+		this.saveToCookieTimeout = setTimeout(() => this.saveToCookie(), 200);
+	}
 
+	restoreFromCookie() {
+		let json = null;
+		(function() {
+			let cookies = decodeURIComponent(document.cookie).split('; ');
+			cookies.forEach(val => {
+				if (val.indexOf(SessionStruct.cookieName) === 0) json = val.substring(SessionStruct.cookieName.length);
+			});
+		})();
+		if (json == null) return;
+
+		this.isReady = json.isReady;
+		this.isLoggedIn = json.isLoggedIn;
+		clearTimeout(this.saveToCookieTimeout);
+	}
+	saveToCookie() {
+		let json = JSON.stringify({
+			isReady: this.isReady,
+			isLoggedIn: this.isLoggedIn
+		});
+		document.cookie = SessionStruct.cookieName + json + "; SameSite=Strict; Secure; max-age=31536000";
 	}
 
 }
 export let Session = new SessionStruct();
-
-async function getSessionCookie() {
-	try {
-		let jwt: string = null;
-		(function() {
-			let name = "socialMediaDemoJwt=";
-			let cookies = decodeURIComponent(document.cookie).split('; ');
-			cookies.forEach(val => {
-				if (val.indexOf(name) === 0) jwt = val.substring(name.length);
-			});
-		})();
-
-		if (jwt == null) return;
-
-		/*let publicKey: jose.KeyLike = null;
-		const { payload, protectedHeader } = await jose.jwtVerify(jwt, publicKey);
-
-		for (let i in payload) {
-			Session[i] = payload[i];
-		}*/
-	} finally {
-		Session.isReady = true;
-	}
-}
-getSessionCookie();
