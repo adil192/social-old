094678cd14be56f651cf4308f506940695c92f55
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-29 10:05:21 +0100

    SocialMediaDemo: make page overlay transition faster

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts(
 );
 
 // Cache name has a timestamp because the browser re-caches the assets when the service worker file is modified
-const staticCacheName = "SocialMediaDemo-static-cache-" + "22-03-29-0942";
+const staticCacheName = "SocialMediaDemo-static-cache-" + "22-03-29-1004";
 const apiUrlPrefix = "https://adil.hanney.org/SocialMediaDemo/api";
 
 // Dexie (IndexedDB)
