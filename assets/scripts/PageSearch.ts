b0d8e1997695ff446610b219a6cc1ca18a2b40f4
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-04-04 02:14:22 +0100

    SMD: PageSearch: focus on searchbox on open

--- assets/scripts/PageSearch.ts
+++ assets/scripts/PageSearch.ts
@@ -1,8 +1,23 @@
 import {Page} from "./Page";
 
 export class PageSearch extends Page {
+	pageSearchInput: HTMLInputElement;
 
 	constructor() {
 		super("pageSearch", "pageOverlaySearch");
+
+		this.pageSearchInput = this.pageElem.querySelector("#pageSearchInput");
+		this.pageSearchInput.onkeyup = () => this.onSearchTermChange();
+	}
+
+	OnOpen() {
+		super.OnOpen();
+
+		this.pageSearchInput.value = "";
+		this.pageSearchInput.focus();
+	}
+
+	onSearchTermChange() {
+
 	}
 }
