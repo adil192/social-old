ef463fbf144fa5d9748a5788132b716e58696a2b
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-04-05 22:34:03 +0100

    SMD: api: Users.Search: exclude current user

--- api/Users.Search.php
+++ api/Users.Search.php
@@ -10,8 +10,8 @@ $searchTerm = $_POST["searchTerm"];
 if (empty($searchTerm)) respond("No search term provided", false);
 
 $stmt = $conn->prepare("SELECT UserId, Username FROM User
-WHERE Username LIKE CONCAT('%', ?, '%') LIMIT 10");
-$stmt->execute([$searchTerm]);
+WHERE Username LIKE CONCAT('%', ?, '%') AND UserId<>? LIMIT 10");
+$stmt->execute([$searchTerm, $_SESSION["userId"]]);
 
 $results = [];
 
