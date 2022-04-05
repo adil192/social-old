7de38c9e80a66cdc79f7c4e024c01cc777c175fe
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-04-05 22:59:28 +0100

    SMD: api: implement Chat.List.php

--- api/Chat.List.php
+++ api/Chat.List.php
@@ -6,18 +6,38 @@ if (!isLoggedIn($conn)) {
 	respond(ErrorMessages::$NotLoggedIn, false);
 }
 
-respond([
-	["Username2", "John Doe", "Sorted :)"],
-	["Username3", "John Doe", "Did you hurt your head?"],
-	["Username4", "John Doe", "Well looky what we have here. No no no, you're staying right here with me."],
-	["Username5", "John Doe", "It was meant to be."],
-	["Username6", "John Doe", "Anyway, if Grandpa hadn't hit him, then none of you would have been born"],
-	["Username7", "John Doe", "I think we need a rematch"],
-	["Username8", "John Doe", "Now look, I think she was born a nun."],
-	["Username9", "John Doe", "What did you sleep in your clothes again last night?"],
-	["Username10", "John Doe", "Hey c'mon, I had to change, you think I'm going back in that zoot suit?"],
-	["Username11", "John Doe", "Well, aren't you going up to the lake tonight, you've been planning it for two weeks"],
-	["Username12", "John Doe", "You'll find out in thirty years!"],
-	["Username13", "John Doe", "Get out of town, I didn't know you did anything creative"],
-	["Username14", "John Doe", "The consequences could be disastrous!"],
-], true);
+// get user's chats' ids
+$stmt = $conn->prepare("SELECT DISTINCT ChatId
+FROM ChatUser
+WHERE UserId=?
+LIMIT 50");
+$stmt->execute([$_SESSION["userId"]]);
+$chatIds = array_map(function ($row) {
+	return "ChatUser.ChatId=" . (int)$row["ChatId"];
+}, $stmt->fetchAll());
+$whereChatIds = implode(" OR ", $chatIds);
+
+// get
+$stmt = $conn->prepare("SELECT ChatId, Username, MessageText
+FROM (
+    SELECT ChatUser.ChatId, User.Username, ChatMessage.MessageText,
+           DENSE_RANK() OVER (PARTITION BY ChatMessage.ChatId ORDER BY ChatMessage.MessageId DESC) AS n
+	FROM ChatUser, User, ChatMessage
+	WHERE ChatUser.UserId <> ? AND ($whereChatIds)
+	  AND ChatUser.UserId = User.UserId
+	  AND ChatMessage.ChatId = ChatUser.ChatId
+	GROUP BY ChatMessage.ChatId
+) AS x
+WHERE n <= 1 LIMIT 50");
+$stmt->execute([$_SESSION["userId"]]);
+
+$results = [];
+
+while ($row = $stmt->fetchObject()) {
+	$results[] = [
+		$row->ChatId,
+		$row->Username,
+		$row->MessageText
+	];
+}
+respond($results, true);
